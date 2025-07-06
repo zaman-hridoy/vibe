ce6c6bbe750c41391e0538e2ad5c3e357831a3c3
zaman_hridoy <amanuzzaman11@gmail.com>
2025-07-06 15:25:47 +0600

    06:e2b-sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,19 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { createAgent, gemini } from "@inngest/agent-kit";
+import "dotenv/config";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world2" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-zaman2");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "code-agent",
       system:
@@ -17,8 +25,13 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`
     );
 
-    console.log(output);
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
 
-    return { output };
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
